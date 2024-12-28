633d3966eed050ead755546be3f5e3dcd7971530
Travis Ennis <travis@travisennis.com>
2024-12-28 15:49:11 -0500

    refactor: fix export issue

--- source/asyncItertools.ts
+++ source/asyncItertools.ts
@@ -3,4 +3,4 @@ export * from "./asyncBatch.ts";
 export * from "./asyncFilter.ts";
 export * from "./asyncMap.ts";
 export * from "./asyncSlidingWindow.ts";
-export * from "./interleave.ts";
+export * from "./asyncInterleave.ts";
--- test/asyncItertools.test.ts
+++ test/asyncItertools.test.ts
@@ -1,23 +1,23 @@
 import assert from "node:assert/strict";
 import { describe, it } from "node:test";
-import * as async from "../source/index.ts";
+import { asyncInterleave } from "../source/index.ts";
 
 describe("asyncIterools", () => {
-	it("interleave should alternate two async iterables", async () => {
-		const iter1 = (async function* () {
-			yield 1;
-			yield 2;
-		})();
+  it("interleave should alternate two async iterables", async () => {
+    const iter1 = (async function* () {
+      yield 1;
+      yield 2;
+    })();
 
-		const iter2 = (async function* () {
-			yield 5;
-		})();
+    const iter2 = (async function* () {
+      yield 5;
+    })();
 
-		const result: number[] = [];
-		for await (const value of async.interleave([iter1, iter2])) {
-			result.push(value);
-		}
+    const result: number[] = [];
+    for await (const value of asyncInterleave([iter1, iter2])) {
+      result.push(value);
+    }
 
-		assert.deepEqual(result, [1, 5, 2]);
-	});
+    assert.deepEqual(result, [1, 5, 2]);
+  });
 });
